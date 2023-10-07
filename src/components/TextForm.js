d56f9b329bf11db8695361a978f71e43c50c63e1
Md. Sabbir Al Mamon <53859124+mdsabbiralmamon@users.noreply.github.com>
2023-10-07 17:32:24 +0600

    Completed Uppercase settings

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-export default function textForm(props) {
+export default function TextForm(props) {
+  const handleOnChange = (event)=>{
+    // console.log("On change triggered")
+    setText(event.target.value);
+  }
+  const handleBtnUppercase = ()=>{
+    // console.log("Convert to uppercase button was clicked");
+    let newText = text.toUpperCase();
+    setText(newText)
+  }
+  const [text, setText] = useState('Enter Text Here');
   return (
     <div>
         <h1>{props.heading}</h1>
         <div className="mb-3">
             <label className="form-label">{props.details}</label>
-            <textarea className="form-control" id="textMainBox" rows="8"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} id="textMainBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary">Convert to Uppercase</button>
+        <button className="btn btn-primary" onClick={handleBtnUppercase}>Convert to Uppercase</button>
     </div>
   )
 }
