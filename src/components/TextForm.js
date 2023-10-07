818ab89149113f97625f0b60b9231a7d56823019
Md. Sabbir Al Mamon <53859124+mdsabbiralmamon@users.noreply.github.com>
2023-10-07 14:27:49 +0600

    modified forms and solved console errors

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,9 +5,10 @@ export default function textForm(props) {
     <div>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <label for="textMainBox" className="form-label">{props.details}</label>
-            <textarea className="form-control" id="textMainBox" rows="3"></textarea>
+            <label className="form-label">{props.details}</label>
+            <textarea className="form-control" id="textMainBox" rows="8"></textarea>
         </div>
+        <button className="btn btn-primary">Convert to Uppercase</button>
     </div>
   )
 }
