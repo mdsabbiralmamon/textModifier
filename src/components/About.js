d7e87791103e76800573b8da4a51a40b389aa3fc
Md. Sabbir Al Mamon <53859124+mdsabbiralmamon@users.noreply.github.com>
2023-10-08 11:12:26 +0600

    Added About Us Page

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec diam tellus, porta in libero lobortis, lobortis laoreet est. Nulla congue commodo diam nec mollis. Proin dapibus porta aliquet. Fusce rutrum massa tristique ante porta varius. Mauris maximus felis eget quam volutpat, eget molestie velit consectetur. Praesent mollis, nibh eu sodales fringilla, sem sapien venenatis odio, sed volutpat eros nunc non felis. Nullam sagittis elit eget nibh euismod rutrum. Fusce sapien felis, pharetra sed leo vitae, porta pharetra urna. Donec commodo, nisi non porta dictum, diam tortor molestie odio, non venenatis metus leo eget quam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nam quis blandit justo. Duis a ligula quis libero consectetur ultrices at eget dolor. Sed finibus nulla lectus, a rhoncus erat dignissim sed. Etiam dui dui, pulvinar ac sollicitudin ut, pretium id tortor. Ut quis blandit lorem. Ut sagittis commodo felis tincidunt sagittis.</p>
+    </div>
+  )
+}
