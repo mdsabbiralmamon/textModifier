e083c6dc9cbd2866c853aecc84a0b1283b5fdf40
Md. Sabbir Al Mamon <53859124+mdsabbiralmamon@users.noreply.github.com>
2023-10-09 17:38:07 +0600

    added dark mode bind with switch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useState } from 'react';
 export const ThemeContext = createContext("null");
 
 function App() {
-  const [ theme, setTheme ] = useState("dark");
+  const [ theme, setTheme ] = useState("light");
 
   const toggleTheme = () =>{
     setTheme((curr) => (curr === "light" ? "dark" : "light"));
@@ -18,6 +18,10 @@ function App() {
       <ThemeContext.Provider value={{ theme, toggleTheme}}>
         <div className="mainApp" id={theme}>
           <Navbar title='Text Modifier' navItem1='Home' navItem2='About us' navItem3='Contact us' />
+          <div class="form-check form-switch mx-3">
+            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onChange={toggleTheme} checked={theme === "dark"} />
+            <label class="form-check-label" for="flexSwitchCheckDefault">Dark Mode</label>
+          </div>
           <TextForm heading ="Paste Your Texts Here" details ="Enter your texts here which you want to modify" />
         </div>
       </ThemeContext.Provider>
