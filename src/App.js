d87d60f55921f4870be2c6d179a1f7ffbfdc9be7
Md. Sabbir Al Mamon <53859124+mdsabbiralmamon@users.noreply.github.com>
2023-10-09 12:25:09 +0600

    added dark mode without button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import { createContext } from 'react';
+import { useState } from 'react';
+
+export const ThemeContext = createContext("null");
 
 function App() {
+  const [ theme, setTheme ] = useState("dark");
+
+  const toggleTheme = () =>{
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
   return (
     <>
-      <Navbar title='Text Modifier' navItem1='Home' navItem2='About us' navItem3='Contact us' />
-      <TextForm heading ="Paste Your Texts Here" details ="Enter your texts here which you want to modify" />
+      <ThemeContext.Provider value={{ theme, toggleTheme}}>
+        <div className="mainApp" id={theme}>
+          <Navbar title='Text Modifier' navItem1='Home' navItem2='About us' navItem3='Contact us' />
+          <TextForm heading ="Paste Your Texts Here" details ="Enter your texts here which you want to modify" />
+        </div>
+      </ThemeContext.Provider>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 
 export default function Navbar(props) {
   return (
+    <>
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
             <a className="navbar-brand" href="/">{props.title}</a>
@@ -28,6 +29,7 @@ export default function Navbar(props) {
             </div>
         </div>
     </nav>
+    </>
   )
 }
 //Declaring Property's Types
